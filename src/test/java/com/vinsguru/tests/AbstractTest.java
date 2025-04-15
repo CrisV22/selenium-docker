730f249d7581b8b2fd1aa52b0f52ede2206d2a16
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-04-15 16:01:25 -0300

    .

--- src/test/java/com/vinsguru/tests/AbstractTest.java
+++ src/test/java/com/vinsguru/tests/AbstractTest.java
@@ -1,20 +1,53 @@
 package com.vinsguru.tests;
 
+import com.vinsguru.listener.TestListener;
+import com.vinsguru.util.Config;
+import com.vinsguru.util.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import com.vinsguru.util.ChromeDriverOptions;
+import org.testng.annotations.Listeners;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Listeners({TestListener.class})
 public abstract class AbstractTest {
-
+    private static final Logger log = LoggerFactory.getLogger(AbstractTest.class);
     protected WebDriver driver;
 
+    @BeforeSuite
+    public void setupConfig() {
+        Config.initialize();
+    }
+
     @BeforeTest
-    public void setDriver(){
+    public void setDriver(ITestContext ctx) throws MalformedURLException {
+        this.driver = Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED)) ? getRemoteDriver() : getLocalDriver();
+        ctx.setAttribute(Constants.DRIVER, this.driver);
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities = ChromeDriverOptions.getChromeDriverOptions();
+        if (Constants.FIREFOX.equalsIgnoreCase(Config.get(Constants.BROWSER))) {
+            capabilities = new FirefoxOptions();
+        }
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+    }
+
+    private WebDriver getLocalDriver() {
         WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver(ChromeDriverOptions.getChromeDriverOptions());
+        return new ChromeDriver(ChromeDriverOptions.getChromeDriverOptions());
     }
 
     @AfterTest
